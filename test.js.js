e9917e0b008cdccff7c4d55a383e0bc0690c721b
Your Name <you@example.com>
2020-07-10 13:55:52 +0900

    git test

---
+++ test.js.js
@@ -0,0 +1,15 @@
+function is_even(num) {
+  
+  if (num % 2 == 0) {
+    return "짝수 입니다"
+    
+  } else {
+    return "짝수가 아닙니다"
+  }
+}
+
+console.log(is_even(11)) // --> "짝수가 아니다"
+console.log(is_even(10)) // "짝수"
+
+
+
